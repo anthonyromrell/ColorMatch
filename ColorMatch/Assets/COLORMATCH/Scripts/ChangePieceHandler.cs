0bfa235ccc16b4d86f150bccff11305238079227
Anthony Romrell <anthonyromrell@mac.com>
2018-12-12 00:11:20 -0700

    Asset Refactor

---
+++ ColorMatch/Assets/COLORMATCH/Scripts/ChangePieceHandler.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class ChangePieceHandler : MonoBehaviour
+{
+    public GameAction ChangeAction;
+    // Start is called before the first frame update
+    void Start()
+    {
+        ChangeAction.Raise += RaiseHandler ;
+    }
+
+    private void RaiseHandler (object obj)
+    {
+        var newPiece = obj as GamePiece;
+        GetComponent<SpriteRenderer>().color = newPiece.ObjColor.Value;
+        
+    }
+}
